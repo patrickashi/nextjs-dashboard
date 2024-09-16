f9690dc276bba86db784c225f300f2e82d382469
Patrick Ashi <patrickpeko1@gmail.com>
2024-09-16 14:27:09 +0100

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return <div>
+        <p> Customers page</p>
+    </div>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return <div>
+        <p> Invoices page</p>
+    </div>
+}
